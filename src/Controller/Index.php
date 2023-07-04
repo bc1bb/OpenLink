b5febe41772fc92705049d20657c87e3edc51639
jusdepatate <jusdepatate@protonmail.com>
2023-07-04 23:39:00 +0200

    support legacy links

--- src/Controller/Index.php
+++ src/Controller/Index.php
@@ -2,16 +2,22 @@
 
 namespace App\Controller;
 
+use \Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\GetVersion;
-
 class Index extends AbstractController
 {
     #[Route('/', methods: ['GET'])]
-    public function index(): Response
+    public function index(Request $request): Response
     {
+        // In case of legacy link
+        $params = $request->query->all();
+        if ($params) {
+            return $this->redirect("/".array_key_first($params), 307);
+        }
+
         // TODO: switch to not "dev"
         $version = GetVersion::get_current_commit("develop", true);
 
